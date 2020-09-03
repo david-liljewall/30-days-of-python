6b02f36b9eb38600a77bbe4a24030291223c52cb
david-liljewall <davidliljewall96@gmail.com>
2020-09-03 11:35:16 -0500

    day 12

---
+++ Day12-Functions/README.md
@@ -0,0 +1,1 @@
+# Welcome to day 12 of the 30 Days of Python series! In this post we’re going to be taking a look at functions
---
+++ Day12-Functions/day12.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#*                                  FUNCTIONS!!                                 #
+# ---------------------------------------------------------------------------- #
+# ------------------------------------ -- ------------------------------------ #
+
+
+
+## Defining our First Function:
+def get_even_numbers( amount ):
+    for number in range( 2, amount + 1, 2 ):
+        print( number )
+        
+# Calling the function:
+get_even_numbers( 22  ) 
+        
+
+#* ---------------------- Specifying multiple parameters ---------------------- #
+
+
