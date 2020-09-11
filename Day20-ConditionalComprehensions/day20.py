c8a982a0e66de9d833a1a6b139c3e10a5429b36b
david-liljewall <davidliljewall96@gmail.com>
2020-09-11 14:20:39 -0500

    day 20 started

---
+++ Day20-ConditionalComprehensions/day20.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------------- #
+#*                  map, filter, and Conditional Comprehensions                 #
+# ---------------------------------------------------------------------------- #
+
+
+
+## Recall a list comprehensions example:
+names = [ "tom", "richard", "harold" ]
+names = [ names.title() for name in names ]
+print( names )
